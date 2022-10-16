c2cac76c72b136cbebb9f605dfb770fa9c3b9244
miguel <miguelcervantes.re@gmail.com>
2022-10-15 19:35:31 -0500

    conjunto

---
+++ conjuntos.js
@@ -0,0 +1,9 @@
+
+
+const familia = [ "miguel" , "maria" ,"marcos" , "lina"]
+
+const familia_set=new Set(familia);
+familia_set.add("miguel");
+familia_set.add("javascript");
+
+console.log(familia_set);
